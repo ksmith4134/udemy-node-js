f25c002029fffee7905a4914ca9793927852371e
ksmith4134 <73805794+ksmith4134@users.noreply.github.com>
2023-10-30 15:42:47 -0400

    added logger middleware

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan')
 
-// load env vars
+// Route files
+const bootcamps = require('./routes/bootcamps');
+
+// Load env vars
 dotenv.config({ path: './config/.env' });
 
 const app = express();
 
+// Dev logging middleware using morgan npm package
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
+
+// Mount bootcamps routers
+// This is done so you dont have to write out this path 
+// for every route in the bootcamps.js file
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
