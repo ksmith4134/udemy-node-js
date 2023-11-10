900b41fc78200aee47d4cea6586634fc416587c5
ksmith4134 <73805794+ksmith4134@users.noreply.github.com>
2023-11-10 15:44:55 -0500

    Added forgot password functionality

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-
+const crypto = require('crypto');
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -38,6 +38,12 @@ const UserSchema = new mongoose.Schema({
 
 // Encrypt password using bcrypt
 UserSchema.pre('save', async function(next){
+
+    if(!this.isModified('password')){
+        next();
+    }
+
+    // only runs if password is modified
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
 });
@@ -52,7 +58,21 @@ UserSchema.methods.getSignedJwtToken = function() {
 // Match user entered password to hashed password in database
 UserSchema.methods.matchPassword = async function(enteredPassword){
     return await bcrypt.compare(enteredPassword, this.password);
-}
+};
+
+// Generate and hash password token
+UserSchema.methods.getResetPasswordToken = function(){
+    // generate token
+    const resetToken = crypto.randomBytes(20).toString('hex');
+
+    // hash token and set to resetPasswordToken field
+    this.resetPasswordToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+
+    // set expire
+    this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
+
+    return resetToken;
+};
 
 
 module.exports = mongoose.model('User', UserSchema);
