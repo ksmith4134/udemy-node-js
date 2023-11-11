72325e180a5ecc6aa31edc14ee1c85e9f24d595d
ksmith4134 <73805794+ksmith4134@users.noreply.github.com>
2023-11-10 23:46:24 -0500

    Added reviews and security

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const morgan = require('morgan');
 const colors = require('colors');
 const fileupload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
@@ -20,7 +25,9 @@ connectDB();
 const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
 const auth = require('./routes/auth');
-
+const users = require('./routes/users');
+const reviews = require('./routes/reviews');
+// need a package to prevent XSS
 
 const app = express();
 
@@ -38,6 +45,26 @@ if(process.env.NODE_ENV === 'development'){
 // file uploading
 app.use(fileupload());
 
+// Sanitize data
+app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+const limiter = rateLimit({
+	windowMs: 10 * 60 * 1000,
+	limit: 100
+})
+
+// Apply the rate limiting middleware to all requests.
+app.use(limiter);
+
+// prevent http param pollution
+app.use(hpp());
+
+// enable CORS
+app.use(cors());
+
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -47,6 +74,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
 app.use('/api/v1/auth', auth);
+app.use('/api/v1/users', users);
+app.use('/api/v1/reviews', reviews);
 
 // Error handler middleware
 app.use(errorHandler);
